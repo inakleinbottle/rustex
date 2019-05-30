bba06345274e8a5406aa29024cbaf245ed49eba0
inakleinbottle <sam@inakleinbottle.com>
2019-05-30 22:25:53 +0100

    Implemented the basic parser

--- src/outparse/src/lib.rs
+++ src/outparse/src/lib.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 use std::fmt;
 use std::error::Error;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
 
 use regex::{Regex, Captures};
 use lazy_static::lazy_static;
 use serde::Serialize;
+
+pub mod report;
+pub use report::*;
 
 lazy_static! {
     static ref ERROR: Regex = Regex::new(
@@ -24,84 +29,52 @@ lazy_static! {
     ).unwrap();
 }
 
-#[derive(Debug, Serialize)]
-pub struct MessageInfo {
-    full: String,
-    details: HashMap<String, String>,
+
+
+
+
+
+
+
+
+struct LogParser<'a, B: BufRead> {
+    report: &'a mut BuildReport,
+    reader: B,
+    last_message: Option<&'a mut Message>,
+    lineno: usize,
+    collect_remaining: Option<usize>,
+    context_lines: usize
 }
 
-#[derive(Debug, Serialize)]
-pub enum Message {
-    Error(MessageInfo),
-    Warning(MessageInfo),
-    Badbox(MessageInfo),
-    Info(MessageInfo),
-}
 
-/*
-impl fmt::Display for Message {
-
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}\n", self.0.full)
-    }
-}
-*/
+impl<'a, B: BufRead> LogParser<'a, B> {
 
 
-
-#[derive(Debug, Serialize)]
-pub struct BuildReport {
-    errors: usize,
-    warnings: usize,
-    badboxes: usize,
-    info: usize,
-    messages: Vec<Message>,
-}
-
-impl BuildReport {
-
-    fn new() -> BuildReport {
-        BuildReport {
-            messages: Vec::new(),
-            errors: 0,
-            warnings: 0,
-            badboxes: 0,
-            info: 0
+    fn next_line(&mut self) -> Option<String> {
+        let mut line = String::new();
+        match self.reader.read_line(&mut line) {
+            Ok(_) => Some(line),
+            Err(_) => None
         }
     }
 
-    fn parse(&mut self, line: &str) {
+    fn parse_line(&mut self, line: &str) {
         if let Some(m) = INFO.captures(&line) {
-            self.info += 1;
-            self.messages.push(BuildReport::process_info(m));
+            self.process_info(m);
         } else if let Some(m) = BADBOX.captures(&line) {
-            self.badboxes += 1;
-            self.messages.push(BuildReport::process_badbox(m));
+            self.process_badbox(m);
         } else if let Some(m) = WARNING.captures(&line) {
-            self.warnings += 1;
-            self.messages.push(BuildReport::process_warning(m));
+            self.process_warning(m);
         } else if let Some(m) = ERROR.captures(&line) {
-            self.errors += 1;
-            self.messages.push(BuildReport::process_error(m));
+            self.process_error(m);
         }
     }
 
-
-    /// Parse a log file to generate a build report.
-    pub fn parse_log<T: AsRef<str>>(buf: &[T]) 
-            -> Result<BuildReport, &'static str> {
-        let mut report = BuildReport::new();
-
-
-
-        
-        Ok(report)
-    }
-
-    fn process_generic(m: Captures) -> MessageInfo {
+    fn process_generic(&mut self, m: Captures) -> MessageInfo {
         let mut info = MessageInfo {
             full: m.get(0).unwrap().as_str().to_owned(),
             details: HashMap::new(),
+            context_lines: Vec::new(),
         };
 
         // 0 - Whole match
@@ -142,15 +115,17 @@ impl BuildReport {
         info
     }
 
-    fn process_info(m: Captures) -> Message {
-        let info = BuildReport::process_generic(m);
-        Message::Info(info)
+    fn process_info(&mut self, m: Captures) {
+        let info = self.process_generic(m);
+        self.report.info += 1;
+        self.report.messages.push(Message::Info(info));
     }
 
-    fn process_badbox(m: Captures) -> Message {
+    fn process_badbox(&mut self, m: Captures) {
         let mut info = MessageInfo {
             full: m.get(0).unwrap().as_str().to_owned(),
-            details: HashMap::new(), 
+            details: HashMap::new(),
+            context_lines: Vec::new(),
         };
 
         // Regex match groups
@@ -205,33 +180,102 @@ impl BuildReport {
             );
         }
 
-        Message::Badbox(info)
+        self.report.badboxes += 1;
+        self.report.messages.push(Message::Badbox(info));
     }
 
-    fn process_warning(m: Captures) -> Message {
-        let info = BuildReport::process_generic(m);
-        Message::Warning(info)
+    fn process_warning(&mut self, m: Captures) {
+        let info = self.process_generic(m);
+        self.report.warnings += 1;
+        self.report.messages.push(Message::Warning(info));
     }
 
-    fn process_error(m: Captures) -> Message {
+    fn process_error(&mut self, m: Captures) {
         if let Some(message) = m.get(5) {
             let mut info = MessageInfo {
                 full: m.get(0).unwrap().as_str().to_owned(),
-                details: HashMap::new()
+                details: HashMap::new(),
+                context_lines: Vec::new(),
             };
 
             info.details.insert(
                 String::from("message"),
                 message.as_str().to_owned()
             );
-            Message::Error(info)
+            self.report.errors += 1;
+            self.report.messages.push(Message::Error(info))
         } else {
-            let info = BuildReport::process_generic(m);
-            Message::Error(info)
+            let info = self.process_generic(m);
+            self.report.errors += 1;
+            self.report.messages.push(Message::Error(info))
         }
     }
 
 }
+
+
+impl<'a, B: BufRead> LogParser<'a, B> {
+
+    pub fn new(report: &'a mut BuildReport, reader: B, context_lines: usize) -> LogParser<'a, B> {
+        LogParser {
+            report,
+            reader,
+            last_message: None,
+            lineno: 0,
+            collect_remaining: None,
+            context_lines,
+        }
+    }
+
+    pub fn parse(&mut self) {
+        loop {
+            let line = match self.next_line() {
+                Some(l) => l,
+                None => continue
+            };
+
+            if let Some(ref mut last) = self.last_message {
+                if let Some(cmpt) = last.get_component_name() {
+                    let pattern = format!("({}) ", cmpt);
+                    if line.starts_with(&pattern) {
+                        let message = line.trim_start_matches(&pattern).trim_start();
+                        last.extend_message(&message);
+                        continue
+                    }
+                } 
+            }
+
+            if let Some(ref mut i) = self.collect_remaining {
+                *i -= 1;
+                if let Some(ref mut last) = self.last_message {
+                    last.add_context(line);
+                }
+                if *i == 0 {
+                    self.collect_remaining = None;
+                }
+                continue
+            }
+
+            self.parse_line(&line);
+
+        }
+    }
+
+}
+
+pub fn parse_log<R: Read>(log: R) -> BuildReport {
+    let mut reader = BufReader::new(log);
+    let mut report = BuildReport::new();
+
+    let mut parser: LogParser<BufReader<R>> = LogParser::new(
+        &mut report, reader, 2
+    );
+
+
+    report
+}
+
+
 
 
 
@@ -241,11 +285,19 @@ impl BuildReport {
 mod tests {
     use super::*;
 
+    fn create_parser(line: &str) -> BuildReport {
+        let mut cursor = io::Cursor::new(&line);
+        let mut reader = BufReader::new(cursor);
+        let mut report = BuildReport::new();
+        let mut parser = LogParser::new(&mut report, reader, 2);
+        parser.parse_line(&line);
+        report
+    }
+
     #[test]
     fn test_underfull_vbox_while_output_active() {
         let line = "Underfull \\vbox (badness 1234) has occurred while \\output is active []";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.badboxes, 1);
         assert_eq!(report.errors, 0);
@@ -257,8 +309,7 @@ mod tests {
     #[test]
     fn test_underfull_vbox_detected_at() {
         let line = "Underfull \\vbox (badness 10000) detected at line 19";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.badboxes, 1);
         assert_eq!(report.errors, 0);
@@ -270,8 +321,7 @@ mod tests {
     #[test]
     fn test_underfull_hbox_at_lines() {
         let line = "Underfull \\hbox (badness 1234) in paragraph at lines 9--10";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.badboxes, 1);
         assert_eq!(report.errors, 0);
@@ -283,10 +333,7 @@ mod tests {
     #[test]
     fn test_overfull_vbox_while_output_active() {
         let line = "Overfull \\vbox (19.05511pt too high) has occurred while \\output is active []";
-
-        let mut report = BuildReport::new();
-
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.badboxes, 1);
         assert_eq!(report.errors, 0);
@@ -299,10 +346,7 @@ mod tests {
     #[test]
     fn test_overfull_hbox_on_line() {
         let line = "Overfull \\hbox (54.95697pt too wide) in paragraph at lines 397--397";
-
-        let mut report = BuildReport::new();
-
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.badboxes, 1);
         assert_eq!(report.errors, 0);
@@ -314,10 +358,7 @@ mod tests {
     #[test]
     fn test_package_not_found_error() {
         let line = "! LaTeX Error: File `foobar.sty' not found.";
-
-        let mut report = BuildReport::new();
-
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
     }
@@ -325,10 +366,7 @@ mod tests {
     #[test]
     fn test_undefined_control_sequence_tex_error() {
         let line = "! Undefined control sequence.";
-
-        let mut report = BuildReport::new();
-
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
     }
@@ -336,8 +374,7 @@ mod tests {
     #[test]
     fn test_too_many_braces_tex_error() {
         let line = "! Too many }'s.";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
         
         assert_eq!(report.errors, 1);
     }
@@ -345,8 +382,7 @@ mod tests {
     #[test]
     fn test_missing_math_mod_text_error() {
         let line = "! Missing $ inserted";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
     }
@@ -354,8 +390,7 @@ mod tests {
     #[test]
     fn test_package_error() {
         let line = "! Package babel Error: Unknown option `latin'. Either you misspelled it";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
     }
@@ -363,8 +398,7 @@ mod tests {
     #[test]
     fn test_pdftex_error() {
         let line = "! pdfTeX error (\\pdfsetmatrix): Unrecognized format..";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
     }
@@ -373,8 +407,7 @@ mod tests {
     #[test]
     fn test_class_error() {
         let line = "! Class article Error: Unrecognized argument for \\macro.";
-        let mut report = BuildReport::new();
-        report.parse(&line);
+        let report = create_parser(&line);
 
         assert_eq!(report.errors, 1);
 
---
+++ src/outparse/src/report.rs
@@ -0,0 +1,109 @@
+use std::collections::HashMap;
+use serde::Serialize;
+
+
+
+#[derive(Debug, Serialize)]
+pub struct MessageInfo {
+    pub full: String,
+    pub details: HashMap<String, String>,
+    pub context_lines: Vec<String>,
+}
+
+impl MessageInfo {
+
+    fn get_component_name<'a>(&'a self) -> Option<&'a str> {
+        if self.details.contains_key("component") {
+            Some(&self.details.get("component").unwrap()) 
+        } else if self.details.contains_key("package") {
+            Some(&self.details.get("package").unwrap())
+        } else if self.details.contains_key("class") {
+            Some(&self.details.get("class").unwrap())
+        } else {
+            None
+        }
+    }
+
+    fn add_context(&mut self, line: String) {
+        self.context_lines.push(line);
+    }
+
+    fn extend_message(&mut self, message: &str) {
+        if let Some(current) = self.details.get_mut("message") {
+            current.push_str(message);
+        } else {
+            self.details.insert(
+                String::from("message"),
+                message.to_owned()
+            );
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub enum Message {
+    Error(MessageInfo),
+    Warning(MessageInfo),
+    Badbox(MessageInfo),
+    Info(MessageInfo),
+}
+
+impl Message {
+    
+    pub(crate) fn get_component_name<'a>(&'a self) -> Option<&'a str> {
+        use Message::*;
+        match self {
+            Error(ref inner) => inner.get_component_name(),
+            Warning(ref inner) => inner.get_component_name(),
+            Info(ref inner) => inner.get_component_name(),
+            Badbox(_) => None
+        }
+    }
+
+    pub(crate) fn extend_message(&mut self, message: &str) {
+        use Message::*;
+        match self {
+            Error(ref mut inner) => inner.extend_message(message),
+            Warning(ref mut inner) => inner.extend_message(message),
+            Info(ref mut inner) => inner.extend_message(message),
+            Badbox(_) => return
+        }
+    }
+
+    pub(crate) fn add_context(&mut self, line: String) {
+        use Message::*;
+        match self {
+            Error(ref mut inner) => inner.add_context(line),
+            Warning(ref mut inner) => inner.add_context(line),
+            Info(ref mut  inner) => inner.add_context(line),
+            Badbox(ref mut inner) => inner.add_context(line)
+        }
+    }
+}
+
+
+
+#[derive(Debug, Serialize)]
+pub struct BuildReport {
+    pub errors: usize,
+    pub warnings: usize,
+    pub badboxes: usize,
+    pub info: usize,
+    pub messages: Vec<Message>,
+}
+
+impl BuildReport {
+
+    pub(crate) fn new() -> BuildReport {
+        BuildReport {
+            messages: Vec::new(),
+            errors: 0,
+            warnings: 0,
+            badboxes: 0,
+            info: 0
+        }
+    }
+
+
+
+}
